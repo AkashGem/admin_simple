8906f041706d0d8d3fe161f465271ab7c5d4578f
AkashGem <aktech508@gmail.com>
2022-04-21 14:52:55 +0530

    Assets folder
    assets

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// Sidebar Action
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sidenav-toggled');
+            // localStorage.setItem('sb-sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+// Feather Icons
+feather.replace()
+
+// Offcanvas
+
+var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+var offcanvasList = offcanvasElementList.map(function(offcanvasEl) {
+    return new bootstrap.Offcanvas(offcanvasEl)
+})
